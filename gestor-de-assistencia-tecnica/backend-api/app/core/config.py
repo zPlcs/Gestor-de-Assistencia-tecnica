5658bcdc0450414c5a18e02e04edabdad7cbb544
Patrik Luiz <111705938+zPlcs@users.noreply.github.com>
2025-10-11 19:18:33 -0300

    All this are deleted

--- gestor-de-assistencia-tecnica/backend-api/app/core/config.py
+++
@@ -1,5 +0,0 @@
-# Configuração Central: Onde as variáveis de ambiente devem ser lidas (idealmente)
-import os
-
-# SUBSTITUA AQUI COM SUAS CREDENCIAIS DO MYSQL
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql://user:password@localhost:3306/assistencia_tecnica_db")
--- gestor-de-assistencia-tecnica/backend-api/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# Este arquivo garante que todos os modelos sejam importados para que
-# o Base.metadata.create_all em main.py saiba quais tabelas criar.
-
-from .cliente import Cliente
-from .funcionario import Funcionario
-from .os import OrdemServico 
-
-# Adicione outros modelos conforme necessário.
